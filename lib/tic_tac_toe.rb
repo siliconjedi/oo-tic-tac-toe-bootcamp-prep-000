46464ce9e223ca31ca2fa7bab439fad099e006cf
Robert D Gauthier <rgauthie@gmail.com>
2018-03-26 17:19:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,6 +79,44 @@ class TicTacToe
     return turns.even? ? "X" : "O"
   end
 
-  
+  def won? (board)
+  WIN_COMBINATIONS.each do |win_combo|
+    win_index_1 = win_combo[0]
+    win_index_2 = win_combo[1]
+    win_index_3 = win_combo[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+      return [win_index_1, win_index_2, win_index_3]
+    elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return [win_index_1, win_index_2, win_index_3]
+    end
+  end
+  return false
+end
+
+def full? (board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+
+def draw? (board)
+  if won?(board)
+    return false
+  elsif full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over? (board)
+  won?(board) || full?(board) || draw?(board)
+end
+
 
 end
